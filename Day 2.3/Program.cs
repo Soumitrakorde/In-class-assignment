ba0486e616905d4d509a915c9ad8f427609d39bf
Saumitra Korde <korde@mail.usf.edu>
2020-01-21 21:01:47 -0500

    Add project files.

---
+++ Day 2.3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Day_2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+            //max heart rate is equal to 220-age
+            int m;
+
+            x = 23;
+            y = 186;
+            m = 220 - x;
+
+            Console.WriteLine(" I am " + x + " years old, my heart rate is " + m + " and I am " + y +  " centimeter tall. ");
+            Console.ReadLine();
+        }
+    }
+}
